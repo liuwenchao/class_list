cd6cf5be7918f3755c11370529993d4a8ce3a9da
Louis Liu <louis.wenchao.liu@gmail.com>
2013-10-11 16:26:13 +0800

    introduce knockoutjs 2.3.0, and use local json file as the remote call doesn't support jsonp.

---
+++ class_list/static/js/class_list.js
@@ -0,0 +1,22 @@
+//FIXME using local json file as the remote call doesn't support jsonp,
+//NOTE: image url could expire, save remote call result as static/class.json to refresh.
+$.getJSON("/static/class.json", function(data) { 
+    var class_list = [];
+    $.each(data.objects, function(index, item){
+        class_list.push({
+            id: item.id, 
+            name: item.name, 
+            teachers: item.teacher_list, 
+            students: item.student_list
+        });
+    });
+    var viewModel = function() {
+        this.classes = class_list;
+        this.chosen_class = ko.observable(class_list[0]);
+        this.switch_class = function(selected_class) {
+            this.chosen_class(selected_class);
+        }.bind(this);
+    };
+
+    ko.applyBindings(new viewModel());
+});
